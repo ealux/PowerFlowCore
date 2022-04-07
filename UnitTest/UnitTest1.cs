96e0aeb532ad5f9cbc6affd5005b09ce7511c6dd
Alexander <Windsl@users.noreply.github.com>
2022-04-07 15:11:03 +0500

    Stage 1.0.04 - Create Test lib

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
