39ee6084c71f697947f3cc2bf4fe6b5ea3dfe14d
Alexander <Windsl@users.noreply.github.com>
2022-04-11 20:27:29 +0500

    Stage 1.0.04 - Add of Two Nodes Test Net
    - Add of Two Nodes Test Net
    - Create of folder for net description files

---
+++ UnitTest/CalcMethods.cs
@@ -0,0 +1,42 @@
+﻿using PowerFlowCore.Data;
+using PowerFlowCore;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathNet.Numerics.LinearAlgebra;
+using Complex = System.Numerics.Complex;
+
+namespace UnitTest
+{
+    public static class CalcMethods
+    {
+        public static bool calc(NetDescription net, double err)
+        {
+            Complex[] U = U = new Complex[net.Nodes.Count];
+            for (int i = 0; i < net.Nodes.Count; i++)
+            {
+                U[i] = (Complex)net.Nodes[i].U;
+            }
+            double[] delta = new double[net.Nodes.Count];
+
+            Engine e = new Engine(net.Nodes, net.Branches);
+            e.Calculate();
+            Vector<Complex> calc = e.desc.U_calc;
+
+            for (int i = 0; i < net.Nodes.Count; i++)
+            {
+                delta[i] = (calc[i].Magnitude - U[i].Magnitude) / U[i].Magnitude;
+            }
+            double maxValue = delta.Max();
+            for (int i = 0; i < net.Nodes.Count; i++)
+            {
+                delta[i] = (calc[i].Phase - U[i].Phase) / U[i].Phase;
+            }
+            double maxValue2 = delta.Max();
+
+            bool res = false;
+            if ((maxValue < err) && (maxValue2 < err)) res = true;
+
+            return res;
+        }
+    }
+}
